a4fd2d16ba628914ad7a9e973f709f91aa042b29
vncp <vincenteatingrice@gmail.com>
2020-12-07 02:41:16 -0800

    Removed km

--- karaoke-simulator/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Dog from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Dog />, document.getElementById('root'));
-registerServiceWorker();
